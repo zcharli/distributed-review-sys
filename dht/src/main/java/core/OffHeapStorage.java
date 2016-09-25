0e0dd67d1fb57f1f615ed8a99b6688f8c7b774ac
Charlie Li <charlie.li@carleton.ca>
2016-09-25 13:22:43 -0400

    Design decisions... how should data be persisted

--- dht/src/main/java/core/OffHeapStorage.java
+++ dht/src/main/java/core/OffHeapStorage.java
@@ -1,6 +1,7 @@
 package core;
 
 import net.tomp2p.dht.Storage;
+import net.tomp2p.dht.StorageMemory;
 import net.tomp2p.peers.Number160;
 import net.tomp2p.peers.Number320;
 import net.tomp2p.peers.Number480;
@@ -17,7 +18,7 @@ import java.util.NavigableMap;
 /**
  * Created by czl on 24/09/16.
  */
-public class OffHeapStorage implements Storage {
+public class OffHeapStorage extends StorageMemory {
 
     // Core
     private static final JedisPool m_storagePool = new JedisPool(new JedisPoolConfig(), "localhost");
@@ -32,7 +33,7 @@ public class OffHeapStorage implements Storage {
     // Core storage
     @Override
     public Data put(Number640 key, Data value) {
-        Data newData = null;
+        Data newData = super.put(key, value);
         try (Jedis adapter = m_storagePool.getResource()) {
 
         }
@@ -41,17 +42,13 @@ public class OffHeapStorage implements Storage {
 
     @Override
     public Data get(Number640 key){
-        return null;
-    }
+        Data onHeap = super.get(key);
+        if (onHeap == null) {
+            try (Jedis adapter = m_storagePool.getResource()) {
 
-    @Override
-    public boolean contains(Number640 key){
-        return true;
-    }
-
-    @Override
-    public int contains(Number640 from, Number640 to){
-        return 0;
+            }
+        }
+        return onHeap;
     }
 
     @Override
@@ -64,10 +61,6 @@ public class OffHeapStorage implements Storage {
         return null;
     }
 
-    public NavigableMap<Number640, Data> subMap(Number640 from, Number640 to, int limit, boolean ascending){
-        return null;
-    }
-
     @Override
     public NavigableMap<Number640, Data> map(){
         return null;
@@ -75,69 +68,6 @@ public class OffHeapStorage implements Storage {
 
     @Override
     public void close(){
-        return;
-    }
-
-    // Maintenance
-    @Override
-    public void addTimeout(Number640 key, long expiration) {
-        return;
-    }
-
-    @Override
-    public void removeTimeout(Number640 key) {
-        return;
-    }
-
-    @Override
-    public Collection<Number640> subMapTimeout(long to) {
-        return null;
-    }
-
-    @Override
-    public int storageCheckIntervalMillis() {
-        return 0;
-    }
-
-    // Domain / entry protection
-    @Override
-    public boolean protectDomain(Number320 key, PublicKey publicKey) {
-        return true;
-    }
-
-    @Override
-    public boolean isDomainProtectedByOthers(Number320 key, PublicKey publicKey) {
-        return true;
-    }
-
-    @Override
-    public boolean protectEntry(Number480 key, PublicKey publicKey) {
-        return true;
-    }
-
-    @Override
-    public boolean isEntryProtectedByOthers(Number480 key, PublicKey publicKey) {
-        return true;
-    }
-
-    // Responsibility
-    @Override
-    public Number160 findPeerIDsForResponsibleContent(Number160 locationKey) {
-        return null;
-    }
-
-    @Override
-    public Collection<Number160> findContentForResponsiblePeerID(Number160 peerID) {
-        return null;
-    }
-
-    @Override
-    public boolean updateResponsibilities(Number160 locationKey, Number160 peerId) {
-        return true;
-    }
-
-    @Override
-    public void removeResponsibility(Number160 locationKey) {
-        return;
+        super.close();
     }
 }
