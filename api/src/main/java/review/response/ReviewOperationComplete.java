2b7d1bc29b3a791e727f892746c64c01268e308f
Charlie Li <charlie.li@carleton.ca>
2016-10-11 11:34:41 -0400

    Making attributes public so the serializer can access it

--- api/src/main/java/review/response/ReviewOperationComplete.java
+++ api/src/main/java/review/response/ReviewOperationComplete.java
@@ -4,8 +4,8 @@ package review.response;
  * Created by cli on 10/10/2016.
  */
 public class ReviewOperationComplete<T> {
-    String status;
-    T message;
+    public String status;
+    public T message;
 
     public ReviewOperationComplete(String status, T message) {
         this.status = status;
