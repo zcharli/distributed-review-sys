ecd95f81541a85b4540c9d52243def448e56b63e
Charlie Li <charlie.li@carleton.ca>
2016-10-24 03:31:18 -0400

    fk tests

--- management/app/pods/product/controller.js
+++ management/app/pods/product/controller.js
@@ -1,7 +1,7 @@
 import Ember from 'ember';
 
 export default Ember.Controller.extend({
-  constants: Ember.service.inject(),
+  constants: Ember.service.inject('constants'),
   reviews: null,
 
   searchSettings: function() {
--- management/tests/unit/pods/product/controller-test.js
+++ management/tests/unit/pods/product/controller-test.js
@@ -3,10 +3,11 @@ import { moduleFor, test } from 'ember-qunit';
 moduleFor('controller:product', 'Unit | Controller | product', {
   // Specify the other units that are required for this test.
   // needs: ['controller:foo']
+  needs: ['service:constants']
 });
 
 // Replace this with your real tests.
 test('it exists', function(assert) {
   let controller = this.subject();
-  assert.ok(controller === null || true);
+  assert.ok(controller);
 });
