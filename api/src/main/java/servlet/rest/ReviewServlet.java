0a644c190fdf8460c978a0285f4b947b18d4415b
Charlie Li <charlie.li@carleton.ca>
2016-10-25 19:34:58 -0400

    acceptance parrallel

--- api/src/main/java/servlet/rest/ReviewServlet.java
+++ api/src/main/java/servlet/rest/ReviewServlet.java
@@ -4,6 +4,7 @@ import com.google.common.base.Strings;
 import com.google.common.collect.ImmutableList;
 import config.DHTConfig;
 import core.DHTManager;
+import core.GlobalContext;
 import error.GenericReply;
 import key.DRSKey;
 import key.DefaultDHTKeyPair;
@@ -13,13 +14,17 @@ import net.tomp2p.peers.Number160;
 import net.tomp2p.peers.Number640;
 import net.tomp2p.storage.Data;
 import org.apache.commons.lang3.text.WordUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import review.BaseReview;
+import review.ProductReviewWrapper;
 import review.ReviewIdentity;
 import review.comparator.ReviewTimestampComparator;
 import review.request.LimitQueryParam;
 import review.response.ReviewGetResponse;
 import review.response.ReviewOperationComplete;
 import validator.ExternalReview;
+import wrapper.ProductRestWrapper;
 
 import javax.ws.rs.*;
 import javax.ws.rs.container.AsyncResponse;
@@ -28,14 +33,20 @@ import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 import java.net.URLDecoder;
 import java.util.*;
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 /**
  * Created by czl on 04/10/16.
  */
 @Path("/review")
 public class ReviewServlet {
+    private final static Logger LOGGER = LoggerFactory.getLogger(ReviewServlet.class);
+    private final ExecutorService m_queryWorker = Executors.newFixedThreadPool(4);
 
-    public ReviewServlet() { }
+    public ReviewServlet() {
+    }
 
     @PUT
     @Path("/new/{m_productName}")
@@ -55,7 +66,7 @@ public class ReviewServlet {
                 throw new NullPointerException("URL decoder failed");
             }
         } catch (Exception e) {
-            response.resume(Response.serverError().entity(new GenericReply<String>("500", "Malformed request parameter, please use utf-8 encoding.")));
+            response.resume(Response.status(Response.Status.BAD_REQUEST).entity(new GenericReply<String>("500", "Malformed request parameter, please use utf-8 encoding.")));
         }
         request.m_productName = productName;
         Number160 locationKey = Number160.createHash(request.getIdentifier());
@@ -67,8 +78,8 @@ public class ReviewServlet {
         // TODO: Validate the identifier
         // request.validateId(identifier);
         DRSKey barcodeKey = DefaultDHTKeyPair.builder()
-                .locationKey( locationKey )
-                .contentKey( contentKey )
+                .locationKey(locationKey)
+                .contentKey(contentKey)
                 .domainKey(DHTConfig.ACCEPTANCE_DOMAIN).build();
         putReview(barcodeKey, request, response);
     }
@@ -78,19 +89,19 @@ public class ReviewServlet {
     @Consumes(MediaType.APPLICATION_JSON)
     @Produces(MediaType.APPLICATION_JSON)
     public void updateReview(final @ExternalReview BaseReview request,
-                                final @Suspended AsyncResponse response,
-                                final @PathParam("id") String identifier) {
+                             final @Suspended AsyncResponse response,
+                             final @PathParam("id") String identifier) {
         if (!identifier.equals(request.getIdentifier()) || request.m_contentId == null || request.m_locationId == null || request.m_domainId == null) {
-            response.resume(Response.serverError().entity(new GenericReply<String>("500", "Invalid request parameters")));
+            response.resume(Response.status(Response.Status.BAD_REQUEST).entity(new GenericReply<String>("500", "Invalid request parameters")));
             return;
         }
 
         // TODO: Validate the identifier and the key to make sure it even exists first
         // request.validateId(identifier);
         DRSKey barcodeKey = DefaultDHTKeyPair.builder()
-                .locationKey( Number160.createHash(request.getIdentifier()) )
-                .contentKey(  Number160.createHash(request.getContent()) )
-                .domainKey( DHTConfig.PUBLISHED_DOMAIN ).build();
+                .locationKey(Number160.createHash(request.getIdentifier()))
+                .contentKey(Number160.createHash(request.getContent()))
+                .domainKey(DHTConfig.PUBLISHED_DOMAIN).build();
 
         DHTManager.instance().getAllFromStorage(barcodeKey, new AsyncResult() {
             @Override
@@ -132,9 +143,9 @@ public class ReviewServlet {
         Number160 newDomainKey = DHTConfig.ACCEPTANCE_DOMAIN;
         Number160 contentKey = Number160.createHash(request.getContent());
         DRSKey reviewKey = DefaultDHTKeyPair.builder()
-                .locationKey( locationKey )
-                .contentKey( contentKey )
-                .domainKey( newDomainKey )
+                .locationKey(locationKey)
+                .contentKey(contentKey)
+                .domainKey(newDomainKey)
                 .build();
         // The original review is updated since the only time editing is allowed is during acceptance.
         Number640 fullKey = new Number640(locationKey, DHTConfig.PUBLISHED_DOMAIN, contentKey, Number160.ZERO);
@@ -164,8 +175,8 @@ public class ReviewServlet {
                           final @Suspended AsyncResponse response) {
 
         DRSKey reviewKey = DefaultDHTKeyPair.builder()
-                .locationKey( Number160.createHash(identifier) )
-                .domainKey( DHTConfig.PUBLISHED_DOMAIN )
+                .locationKey(Number160.createHash(identifier))
+                .domainKey(DHTConfig.PUBLISHED_DOMAIN)
                 .build();
         DHTManager.instance().getAllFromStorage(reviewKey, new AsyncResult() {
             @Override
@@ -182,14 +193,14 @@ public class ReviewServlet {
                 List<BaseReview> allReviews = new ArrayList<BaseReview>();
                 List<BaseReview> limitedReviews = new LinkedList<BaseReview>();
                 for (Map.Entry<Number640, Data> results : payload().entrySet()) {
-                    allReviews.add(((ReviewIdentity)(results.getValue().object())).identity());
+                    allReviews.add(((ReviewIdentity) (results.getValue().object())).identity());
                 }
 
                 Collections.sort(allReviews, new ReviewTimestampComparator());
 
                 int max = limit.page * limit.step;
                 int start = (limit.page - 1) * limit.step;
-                if ( max > allReviews.size() ) {
+                if (max > allReviews.size()) {
                     max = allReviews.size();
                 }
                 for (int i = start; i < max; i++) {
@@ -210,20 +221,47 @@ public class ReviewServlet {
         if (trackedIds == null) {
             response.resume(Response.serverError().entity(new GenericReply<String>("DHT-APPROVAL", "Unabled to find tracked IDs on this node.")));
         }
-        List<BaseReview> results = new LinkedList<>();
-        for (Number160 locationId : trackedIds) {
-            DRSKey instanceKey = DefaultDHTKeyPair.builder().locationKey(locationId).domainKey(DHTConfig.ACCEPTANCE_DOMAIN).build();
-            for (Data result : DHTManager.instance().getAllFromStorage(instanceKey)) {
-                try {
-                    results.add(((ReviewIdentity)result.object()).identity());
-                } catch (Exception e) {
-                    response.resume(Response.serverError().entity(
-                            new GenericReply<String>("DHT-APPROVAL", "Failed to extract data from DHT.")
-                    ).build());
-                }
-            }
-        }
-        response.resume(Response.ok().entity(new ReviewOperationComplete<List<BaseReview>>("200",results)).build());
+//        List<BaseReview> results = new LinkedList<>();
+//        for (Number160 locationId : trackedIds) {
+//            DRSKey instanceKey = DefaultDHTKeyPair.builder().locationKey(locationId).domainKey(DHTConfig.ACCEPTANCE_DOMAIN).build();
+//            for (Data result : DHTManager.instance().getAllFromStorage(instanceKey)) {
+//                try {
+//                    results.add(((ReviewIdentity)result.object()).identity());
+//                } catch (Exception e) {
+//                    response.resume(Response.serverError().entity(
+//                            new GenericReply<String>("DHT-APPROVAL", "Failed to extract data from DHT.")
+//                    ).build());
+//                }
+//            }
+//        }
+//        response.resume(Response.ok().entity(new ReviewOperationComplete<List<BaseReview>>("200",results)).build());
+        final List<BaseReview> reviewsInAcceptance = new LinkedList<>();
+        final CompletableFuture<?>[] allAcceptanceReviews = trackedIds.stream()
+                .map(key -> CompletableFuture.runAsync(() -> {
+                    final Collection<Data> reviews = DHTManager.instance()
+                            .getAllFromStorage(DefaultDHTKeyPair.builder()
+                                    .locationKey(key)
+                                    .domainKey(DHTConfig.ACCEPTANCE_DOMAIN)
+                                    .build());
+                    if (reviews != null) {
+                        reviews.forEach(review -> {
+                            try {
+                                final BaseReview basePointer = (BaseReview) review.object();
+                                reviewsInAcceptance.add(basePointer);
+                            } catch (Exception e) {
+                                LOGGER.error("Exception when trying to retrieve acceptance review object from Data: " + e.getMessage());
+                            }
+                        });
+                    }
+                }, m_queryWorker).exceptionally(ex -> {
+                    LOGGER.error("An error occured when fetch all acceptance reviews from locations: " + ex.getMessage());
+                    ex.printStackTrace();
+                    return null;
+                }))
+                .toArray(CompletableFuture[]::new);
+        CompletableFuture.allOf(allAcceptanceReviews).join();
+        response.resume(Response.ok().entity(new ReviewGetResponse(200, reviewsInAcceptance)).build());
+
     }
 
     @GET
