60ec8850f2d0a8c70ac6c9e87a83a0a3b82b100b
Charlie Li <charlie.li@carleton.ca>
2016-10-26 03:40:54 -0400

    test fix

--- management/tests/integration/pods/components/approval-form/component-test.js
+++ management/tests/integration/pods/components/approval-form/component-test.js
@@ -1,5 +1,4 @@
 import { moduleForComponent, test } from 'ember-qunit';
-import hbs from 'htmlbars-inline-precompile';
 
 moduleForComponent('approval-form', 'Integration | Component | approval form', {
 
