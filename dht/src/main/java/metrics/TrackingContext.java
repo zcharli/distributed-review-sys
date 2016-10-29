3a75e77dfac6626921376bd5a7a5424192f077c6
Charlie Li <charlie.li@carleton.ca>
2016-10-29 01:18:13 -0400

    Added tracking

--- dht/src/main/java/metrics/TrackingContext.java
+++ dht/src/main/java/metrics/TrackingContext.java
@@ -1,5 +1,7 @@
 package metrics;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonProperty;
 import config.DHTConfig;
 import net.tomp2p.peers.Number160;
 
@@ -7,7 +9,8 @@ import net.tomp2p.peers.Number160;
  * Created by cli on 10/10/2016.
  */
 public class TrackingContext implements TrackingContextView {
-    public Number160 locationKey;
+    @JsonIgnore
+    public int[] locationKey;
     public long numberOfHits;
     public String trackedDomain;
     public MetricsCollector.TrackingType trackingType;
@@ -16,13 +19,13 @@ public class TrackingContext implements TrackingContextView {
     public TrackingContext() { }
 
     public TrackingContext(Number160 locationBuf, MetricsCollector.TrackingType type) {
-        locationKey = locationBuf;
+        locationKey = locationBuf.toIntArray();
         numberOfHits = 1;
         trackedDomain = DHTConfig.MY_DOMAIN;
         trackingType = type;
     }
     public Number160 getPrimary() {
-        return locationKey;
+        return new Number160(locationKey);
     }
 
     public MetricsCollector.TrackingType getType() {
